7b58db9aecb8738e33a2296339568ac53315cfd0
nitika <nitikadiwakar.bajaj@gmail.com>
2023-02-12 14:37:09 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let dollarCurrency = document.getElementById(dollar)
+let poundCurrency = document.getElementById(pound)
+
+function submit() {
+	console.log('test')
+	// enter value to execute on the screen and store in variable"input value"
+	let inputValue = dollar.value
+	// console.log(inputValue)
+	let convert = inputValue * 0.77
+	pound.innerHTML = convert
+}
+function clean() {
+	// console.log('test')
+	let input = document.getElementById('dollar')
+	input.value = ''
+	let output = document.getElementById('pound')
+	output.value = ''
+}
